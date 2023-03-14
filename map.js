60cf0f4c6e8e6829a1cb57b1e57d3b78efcca46c
1ithiA <69569705+1ithiA@users.noreply.github.com>
2023-03-14 13:02:43 +0700

    Add files via upload

--- map.js
+++ map.js
@@ -1,6 +1,174 @@
-infoWindow.open(map);
-  
-var UserPos = new google.maps.Marker({
-  position: ,
-  map: map,
+function myMap() {
+  var mapProp= {
+  center:new google.maps.LatLng(13.738649625233439, 100.5324212405303),
+  zoom:17,
+};
+
+/*Coordinates*/
+var point1 = { lat: 13.735936017497728, lng: 100.53071747155853};
+var point2 = { lat: 13.736522929590551, lng: 100.52953254883069};
+var point3 = { lat: 13.737257102262152, lng: 100.52858397219916};
+var point4 = { lat: 13.738885597064707, lng: 100.52844705631053};
+var point5 = { lat: 13.740030280173663, lng: 100.52889362961828};
+var point6 = { lat: 13.73972712877361, lng: 100.532117338905};
+var point7 = { lat: 13.739521656258006, lng: 100.53230795763523};
+var point8 = { lat: 13.737094177735804, lng: 100.53186628737458};
+var point9 = { lat: 13.736474278162431, lng: 100.53169557601313};
+
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker1 = new google.maps.Marker({
+position: point1,
+map: map,
+icon:"icon/1.png",
 });
+var marker2 = new google.maps.Marker({
+position: point2,
+map: map,
+icon:"icon/2.png",
+});
+var marker3 = new google.maps.Marker({
+position: point3,
+map: map,
+icon:"icon/3.png",
+
+});
+var marker4 = new google.maps.Marker({
+position: point4,
+map: map,
+icon:"icon/4.png",
+});
+var marker5 = new google.maps.Marker({
+position: point5,
+map: map,
+icon:"icon/5.png",
+});
+var marker6 = new google.maps.Marker({
+position: point6,
+map: map,
+icon:"icon/6.png",
+});
+
+var marker7 = new google.maps.Marker({
+position: point7,
+map: map,
+icon:"icon/7.png",
+});
+var marker8 = new google.maps.Marker({
+position: point8,
+map: map,
+icon:"icon/8.png",
+});
+var marker9 = new google.maps.Marker({
+position: point9,
+map: map,
+icon:"icon/9.png",
+});
+
+/*google.maps.event.addListener(map, 'click', function(event) {
+placeMarker(map, event.latLng);
+});
+
+function placeMarker(map, location) {
+var marker = new google.maps.Marker({
+position: location,
+map: map
+});
+var infowindow = new google.maps.InfoWindow({
+content: 'Latitude: ' + location.lat() +
+'<br>Longitude: ' + location.lng()
+});
+infowindow.open(map,marker);
+}*/
+
+///Route Liner///
+var i1_1 = {lat: 13.736231863900983, lng: 100.53076185837237}
+var i1_2 = {lat: 13.736397314327107, lng: 100.5296838963318}
+var myTrip = [point1,i1_1, i1_2, point2];
+var flightPath = new google.maps.Polyline({
+path:myTrip,
+map:map,
+strokeColor:"#0000FF",
+strokeOpacity:0.8,
+strokeWeight:5
+});
+
+var i2_1 = {lat:13.736597451858149, lng:100.52913127392993}
+var i2_2 = {lat:13.736471085488146, lng:100.52911786288453}
+var i2_3 = {lat:13.736562825110644, lng:100.52865215690575}
+var i2_4 = {lat:13.736689667174902, lng:100.52859940195353}
+var i2_5 = {lat:13.736732322257097, lng:100.52848430707753}
+
+var myTrip = [point2,i2_1,i2_2,i2_3,i2_4,i2_5,point3];
+var flightPath = new google.maps.Polyline({
+path:myTrip,
+map:map,
+strokeColor:"#000",
+strokeOpacity:0.8,
+strokeWeight:5
+});
+
+var i3_1 = {lat:13.73806164092444, lng:100.52874698414377}
+var i3_2 = {lat:13.73814297523583, lng:100.52812339739197}
+var i3_3 = {lat:13.738697596876056, lng:100.52823344211511}
+var i3_4 = {lat:13.738673666094055, lng:100.52841965700826}
+var myTrip = [point3,i3_1,i3_2,i3_3,i3_4,point4];
+var flightPath = new google.maps.Polyline({
+path:myTrip,
+map:map,
+strokeColor:"#09C917",
+strokeOpacity:0.8,
+strokeWeight:5
+});
+
+///Location finder///
+infoWindow = new google.maps.InfoWindow();
+
+const locationButton = document.createElement("button");
+
+locationButton.textContent = "Pan to Current Location";
+locationButton.classList.add("custom-map-control-button");
+map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+locationButton.addEventListener("click", () => {
+// Try HTML5 geolocation.
+if (navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(
+(position) => {
+const pos = {
+lat: position.coords.latitude,
+lng: position.coords.longitude,
+};
+
+
+infoWindow.setPosition(pos);
+infoWindow.setContent("Location found.");
+infoWindow.open(map);
+map.setCenter(pos);
+var UPos = new google.maps.Marker({
+position: pos,
+map: map,
+icon:"C:/Users/fonpa/Desktop/CUmusicwalk/clef.png",
+animation:google.maps.Animation.BOUNCE
+});
+},
+() => {
+handleLocationError(true, infoWindow, map.getCenter());
+}
+);
+} else {
+// Browser doesn't support Geolocation
+handleLocationError(false, infoWindow, map.getCenter());
+}
+});
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+infoWindow.setPosition(pos);
+infoWindow.setContent(
+browserHasGeolocation
+? "Error: The Geolocation service failed."
+: "Error: Your browser doesn't support geolocation."
+);
+
+
+}
